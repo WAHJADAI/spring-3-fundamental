374dbbf13ae626a110d1b573d1376c4e26c98435
Harin3Bone <harin.thananam@gmail.com>
2023-12-14 22:29:33 +0700

    API Concept & Design

---
+++ src/main/java/com/tutorial/springfundamental/controller/KeyboardController.java
@@ -0,0 +1,45 @@
+package com.tutorial.springfundamental.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/keyboard")
+public class KeyboardController {
+
+    @GetMapping(value = "")
+    @ResponseStatus(HttpStatus.OK)
+    public String getAllKeyboard() {
+        return "GET All Keyboard";
+    }
+
+    @GetMapping(value = "/id")
+    @ResponseStatus(HttpStatus.OK)
+    public String getKeyboardById() {
+        return "GET Keyboard by id";
+    }
+
+    @PostMapping(value = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createKeyboard() {
+        return "POST Keyboard";
+    }
+
+    @PutMapping(value = "")
+    @ResponseStatus(HttpStatus.OK)
+    public String updateKeyboard() {
+        return "PUT Keyboard";
+    }
+
+    @DeleteMapping(value = "")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteKeyboard() {
+        // statement
+    }
+}
