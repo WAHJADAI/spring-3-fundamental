251db7c1bce6493890e1b790ba8a3bf53d242a36
WAHJADAI <drafzaza123@icloud.com>
2023-12-20 20:09:24 +0700

    create service create, update and delete

--- src/main/java/com/tutorial/springfundamental/service/KeyboardService.java
+++ src/main/java/com/tutorial/springfundamental/service/KeyboardService.java
@@ -1,10 +1,13 @@
 package com.tutorial.springfundamental.service;
 
+import com.tutorial.springfundamental.dto.KeyboardRecord;
 import com.tutorial.springfundamental.model.KeyboardModel;
 import com.tutorial.springfundamental.repository.KeyboardRepository;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,5 +24,26 @@ public class KeyboardService {
                 .orElseThrow();
     }
 
+    public KeyboardModel saveKeyboard(KeyboardRecord request){
+        var keyboard =new KeyboardModel();
+        keyboard.setName(request.name());
+        keyboard.setQuantity(request.quantity());
+        keyboard.setPrice(BigDecimal.valueOf(request.price()));
+        keyboardRepository.save(keyboard);
+      return keyboard;
+    }
 
+    public KeyboardModel updateKeyboard(String id,KeyboardRecord request){
+        var existingKeyboard = getKeyboardById(id);
+        existingKeyboard.setName(request.name());
+        existingKeyboard.setQuantity(request.quantity());
+        existingKeyboard.setPrice(BigDecimal.valueOf(request.price()));
+        keyboardRepository.save(existingKeyboard);
+        return existingKeyboard;
+    }
+    public void deleteKeyboard(String id){
+        var existingKeyboard = getKeyboardById(id);
+        keyboardRepository.delete(existingKeyboard);
+
+    }
 }
