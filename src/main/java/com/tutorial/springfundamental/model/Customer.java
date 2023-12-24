54c5fb0534dae949ca6c9e491a38cae6f8528108
wahjadai <drafzaza123@icloud.com>
2023-12-24 13:52:32 +0700

    create entity customer

---
+++ src/main/java/com/tutorial/springfundamental/model/Customer.java
@@ -0,0 +1,32 @@
+package com.tutorial.springfundamental.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name="customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+    @Column(name = "username")
+    private  String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+    @CreationTimestamp
+    @Column(name = "created_timestamp")
+    private ZonedDateTime createTimestamp;
+    @UpdateTimestamp
+    @Column(name = "last_updated_timestamp")
+    private ZonedDateTime lastUpdateTimestamp;
+}
