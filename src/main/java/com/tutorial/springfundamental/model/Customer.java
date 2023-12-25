bfc160b415e42c495c7f8355c41eef1d4f2d6d15
WAHJADAI <drafzaza123@icloud.com>
2023-12-25 19:35:16 +0700

    validation uuid and password

--- src/main/java/com/tutorial/springfundamental/model/Customer.java
+++ src/main/java/com/tutorial/springfundamental/model/Customer.java
@@ -1,6 +1,7 @@
 package com.tutorial.springfundamental.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -18,18 +19,26 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
+    @Pattern(regexp = "^[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}$")
     private UUID id;
+
     @Column(name = "username")
     private  String username;
+
     @Column(name = "password")
+    @Pattern(regexp = "(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+!=])[A-Za-z[0-9]$@$!%*?&]{8,24}$")
     private String password;
+
     @Column(name = "email")
     private String email;
+
     @Column(name = "date_of_birth")
     private Date dateOfBirth;
+
     @CreationTimestamp
     @Column(name = "created_timestamp")
     private ZonedDateTime createTimestamp;
+
     @UpdateTimestamp
     @Column(name = "last_updated_timestamp")
     private ZonedDateTime lastUpdateTimestamp;
